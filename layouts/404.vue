40122096ccc6af4b3f6dab25929c9cfe24f7c261
ascii13 <476747909@qq.com>
2021-02-20 18:03:38 +0800

    add home and 404 page

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="error">
-        <div>404</div>
+        <img :src="require('../assets/404.svg')" />
         <div>Looks like we've got some broken links.</div>
         <router-link to="/">Take me home</router-link>
     </div>
@@ -13,7 +13,11 @@
     justify-content: center;
     align-items: center;
     height: 100vh;
-    font-size: 2rem;
-    & > :not(:first-child)
+    font-size: 1.5rem;
+
+    & > :last-child
         margin-top: 2rem;
+        
+    & > img
+        width: 300px;
 </style>
--- layouts/Home.vue
+++ layouts/Home.vue
@@ -1,25 +1,34 @@
 <template>
     <div class="home">
         <div class="header">
-            <div class="title">{{ title }}</div>
+            <div v-if="$site.title" class="title">{{ $site.title }}</div>
             <div class="shortcuts">
                 <router-link class="shortcut" v-for="(item, index) in shortcuts" :key="index" :to="item.link">{{ item.text }}</router-link>
             </div>
         </div>
-        <div class="content"></div>
+        <div class="content">
+            <img v-if="$themeConfig.avatar" class="avatar" :src="$themeConfig.avatar" />
+            <div v-if="$themeConfig.nickname" class="nickname">{{ $themeConfig.nickname }}</div>
+            <div v-if="$themeConfig.motto" class="motto">{{ $themeConfig.motto }}</div>
+            <div v-if="$themeConfig.externalLinks" class="external-link">
+                <a v-for="(item, index) in $themeConfig.externalLinks" :key="index" :href="item.link" target="_blank"><img :src="item.iconPath" /></a>
+            </div>
+        </div>
+        <div class="footer">
+            <div>© {{ $themeConfig.nickname }} | Powered by 
+                <a target="_blank" href="https://vuepress.vuejs.org/">VuePress</a> & 
+                <a target="_blank" href="https://github.com/ASCII13/Explorer">Explorer</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     computed: {
-        title() {
-            return this.$site.title;
-        },
         shortcuts() {
-            console.log(`获取到的themeConfig是${JSON.stringify(this.$themeConfig)}`);
             return this.$themeConfig.shortcuts;
-        }
+        },
     }
 }
 </script>
@@ -30,7 +39,7 @@ $spacing = 20rem;
 .home
     width: 100vw;
     height: 100vh;
-    background-color: gray;
+    background-color: white;
 
 .header
     display: flex;
@@ -38,12 +47,12 @@ $spacing = 20rem;
     box-sizing: border-box;
     height: 66px;
     padding: 10px;
-    background-color: indianred;
     font-size: 20px;
 
 .title
     margin-left: $spacing;
     line-height: 46px;
+    font-size: 25px;
 
 .shortcuts
     margin-right: $spacing;
@@ -57,5 +66,45 @@ $spacing = 20rem;
     text-decoration: none;
 
 .content
-    min-height: calc(100vh - 66px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 66px - 6rem);
+
+    & > div
+        margin-top: 0.8rem;
+
+.avatar
+    height: 128px;
+    border-radius: 50%;
+    background-color: wheat;
+    box-shadow: 0 0 0 0.22em #F2F6FC;
+
+.nickname
+    font-size: 1.5rem;
+
+.motto
+    font-size: 1.1rem;
+
+.external-link
+    & img
+        width: 32px;
+        height: 32px;
+    
+    & > a:not(:last-child)
+        margin-right: 1rem;
+
+.footer
+    height: 6rem;
+    line-height: 6rem;
+    text-align: center;
+    font-size: 1rem;
+
+    & a
+        color: black;
+        text-decoration: none;
+    
+    & a:hover
+        text-decoration: underline;
 </style>
